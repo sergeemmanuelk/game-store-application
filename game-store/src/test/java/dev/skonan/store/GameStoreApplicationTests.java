362ab0c5d725927507e74691b22d58a63ebd5eeb
sergeemmanuelk <serge_em@outlook.fr>
2024-12-17 13:28:05 +0000

    initializing the backend project

---
+++ game-store/src/test/java/dev/skonan/store/GameStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.skonan.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
